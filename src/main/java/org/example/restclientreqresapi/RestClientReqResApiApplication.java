47d42aa2564b6fccc905d29ca8e7810928424908
Yaroslava <yaroslava.moskalova@gmail.com>
2025-09-18 15:49:38 +0200

    project initialisation

---
+++ src/main/java/org/example/restclientreqresapi/RestClientReqResApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.restclientreqresapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientReqResApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientReqResApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/restclientreqresapi/RestClientReqResApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restclientreqresapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientReqResApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
